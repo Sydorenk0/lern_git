ee1af30b043f4c2fa5da7b31843ffdcada696be8
Sydorenk0 <alexandr.sydorenko@gmail.com>
2016-03-06 15:06:06 +0200

    Внес изм в 2 файлах кимичу только один YYY.

--- src/main/java/yyy.java
+++ src/main/java/yyy.java
@@ -2,8 +2,11 @@
  * Created by Alex on 04/03/2016.
  */
 public class yyy {
-    public static void  main(String[] args){
+    public static void  main(String[] args)
+    {
     System.out.println("ggghgh");
+//        коментарий
+//        еще раз
     }
 }
 
